d88c24434ee554c7994beac0287b47acde009c98
Mehdi ASSAS <meassas@e1r13p6.42.fr>
2017-01-27 23:44:55 +0100

    push01

---
+++ gnl/gnl/a01/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meassas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 16:33:42 by meassas           #+#    #+#             */
+/*   Updated: 2017/01/27 02:57:11 by meassas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 40
+# define ENDL 0x0a
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int		get_next_line(int const fd, char **line);
+
+#endif
