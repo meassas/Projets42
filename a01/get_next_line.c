b81394bb1703cb3d87bd96a5ef9c5d2074a89ca1
Mehdi ASSAS <meassas@e1r4p16.42.fr>
2017-02-08 21:54:40 +0100

    gnlvalidbutleaks

--- a01/get_next_line.c
+++ a01/get_next_line.c
@@ -6,10 +6,11 @@
 /*   By: meassas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 13:54:42 by meassas           #+#    #+#             */
-/*   Updated: 2017/02/03 05:53:02 by meassas          ###   ########.fr       */
+/*   Updated: 2017/02/08 20:49:58 by meassas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "get_next_line.h"
 
 static	int		ft_strgnlen(char *str)
@@ -26,26 +27,31 @@ static char		*get_line(char *buff, char *stock)
 {
 	int len;
 	char *tmpline;
+	char *tmp;
 
 	len = 0;
 	if (buff)
-	{
-		len = ft_strgnlen(buff);
-		//ft_putstr("len buff = ");
-		//ft_putnbr(len);
-	}
-	//ft_putstr("len stock = ");
-	//if (ft_strgnlen(stock) == 0)
-	//	return (tmpline);
+		len = ft_strlen(buff);
 	len += ft_strgnlen(stock);
-	//ft_putnbr(len);
 	tmpline = ft_memalloc(len + 1);
-	//if (!len)
-	//	return (tmpline);
+	//tmpline = NULL;
 	if (buff)
+	{
 		tmpline = ft_strncat(buff, stock, ft_strgnlen(stock));
+		//free(tmpline);
+		//tmpline = ft_memmove(tmpline, stock, ft_strgnlen(stock) + ft_strlen(buff));
+	}
 	else
-		tmpline = ft_strncpy(tmpline, stock, ft_strgnlen(stock));
+	{
+		//tmpline = ft_memcpy(tmpline, stock, ft_strgnlen(stock));
+		tmp = tmpline;
+		ft_memdel((void**)&tmpline);
+		ft_memdel((void**)tmpline);
+		tmpline = ft_memmove(tmp, stock, ft_strgnlen(stock));
+		//ft_memdel((void**)tmp);
+		//ft_memdel((void**)&tmp);
+		//free(tmpline);
+	}
 	return (tmpline);
 }
 
@@ -55,45 +61,40 @@ int		get_next_line(int const fd, char **line)
 	static char *stock = NULL;
 	char *buffer;
 
-	*line = NULL;
-	buffer = ft_memalloc(BUFF_SIZE + 1);
-	//ft_putendl("GNl");
 	if (!line || fd < 0)
 		return (-1);
+	buffer = NULL;
+	*line = NULL;
+	buffer = ft_memalloc(BUFF_SIZE + 1);
 	if (stock)
 	{
-		//ft_putendl("stock != NULL2");
-		*line = get_line(*line, stock + 1);
-		stock = ft_strchr(stock + 1, '\n');
+		if (stock + 1)
+		{
+			*line = get_line(*line, stock + 1);
+			stock = ft_strchr(stock + 1, '\n');
+		}
 		if (stock && (*stock == '\n'))
 			return (1);
-		else if (stock == NULL && (stock = ft_strchr(buffer, '\0')))
+		ft_memdel((void**)stock);
+		ft_memdel((void**)&stock);
+		//free(stock);
+		/*else if (stock == NULL && (stock = ft_strchr(buffer, '\0')))
 		{
-		//	ft_putendl("stock == NULL");
 			*line = ft_strjoin(*line, stock);
-			free(stock);
+			//free(stock);
 			//free(buffer);
-			stock = ft_memalloc(BUFF_SIZE + 1);
+			//stock = ft_memalloc(BUFF_SIZE + 1);
+			//return (get_next_line(fd, line));
 			//buffer = ft_memalloc(BUFF_SIZE + 1);
 			//stock = ft_strchr(buffer, '\0');
-		}
-		//ft_putendl("stock != 99");
-//stock = ft_memalloc(BUFF_SIZE + 1);
-		//ft_putendl("stock1 = ");
-		//ft_putendl(stock);
-		//ft_putstr("line = ");
-		//ft_putendl(*line);
-		/*if (stock == NULL)
-		{
-			//stock = ft_strchr(stock, '\0'))
-			*line = get_line(*line, stock);
 		}*/
 	}
-	if (stock == NULL)
+	if (stock == NULL && *line == NULL)
 	{
-		//ft_putendl("stock == null");
+	//	free(buffer);
 		stock = ft_memalloc(BUFF_SIZE + 1);
 	}
+	//if ((buffer == NULL && *line))
 	while ((ft_strchr(buffer, '\n')) == NULL && *line)
 	{
 		if ((ret = read(fd, buffer, BUFF_SIZE)) < 1 && *line)
@@ -102,36 +103,41 @@ int		get_next_line(int const fd, char **line)
 				return (-1);
 			if (ret == 0 && ((**line != '\0')))
 			{
-				free(stock);
-				free(buffer);
-				//ft_putendl("yoyo");
+	//			ft_memdel((void**)&buffer);
 				return (1);
 			}
-			if (ret == 0/* && *line == NULL*/)
+			if (ret == 0 && **line == '\0')
 			{
-				//ft_putendl("yo");
+				ft_memdel((void**)&stock);
+	//			ft_memdel((void**)&buffer);
+				ft_memdel((void**)stock);
+	//			ft_memdel((void**)buffer);
+				//ft_memdel((void**)line);
+				//ft_memdel((void*)*line);
+		//		ft_putendl("yo");
 				return (0);
 			}
 		}
 		buffer[ret] = '\0';
 		stock = ft_strchr(buffer, '\n');
 		*line = get_line(*line, buffer);
-		//free(buffer);
-		//ft_putendl("buffer");
 		if (stock && (*stock == '\n'))
-			return (1);
-		//ft_putendl("buffer2");
-		/*if (stock == NULL && (stock = ft_strchr(buffer, '\0')))
 		{
-			//ft_putendl("stock == NULL");
-			*line = ft_strjoin(*line, buffer);
-			//free(stock);
+			return (1);
+		}
+	//	ft_memdel((void**)&stock);
+	//	free(stock);
+		/*if (stock == NULL && *line)
+		{
+		//	*line = ft_strjoin(*line, buffer);
 			//free(buffer);
-			//stock = ft_memalloc(BUFF_SIZE + 1);
-			//buffer = ft_memalloc(BUFF_SIZE + 1);
-			//stock = ft_strchr(buffer, '\0');
+			//ft_memdel((void**)buffer);
+			stock = ft_memalloc(BUFF_SIZE + 1);
 		}*/
 	}
+	//ft_memdel((void**)&stock);
+	//free(stock);
+	//return (1);
 	return (get_next_line(fd, line));
 }
 
@@ -143,9 +149,9 @@ int		get_next_line(int const fd, char **line)
 	int ret;
 	char *stock;
 
-	fd = open(av[1], O_RDONLY);
 	if (line)
 		free(line);
+	fd = open(av[1], O_RDONLY);
 	while ((ret = get_next_line(fd, &line)))
 	{
 		ft_putstr("line = ");
--- a01/get_next_line.h
+++ a01/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: meassas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 16:33:42 by meassas           #+#    #+#             */
-/*   Updated: 2017/02/03 05:49:46 by meassas          ###   ########.fr       */
+/*   Updated: 2017/02/08 20:06:07 by meassas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 40
+# define BUFF_SIZE 1
 # define ENDL 0x0a
 
 #include <stdio.h>
